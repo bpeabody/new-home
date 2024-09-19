37cd0d8a236e9ed8c3654b4f63963f6c1713969f
Brock Peabody <brock.peabody@gmail.com>
2024-09-18 23:39:36 -0500

    added ripgrep

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -6,6 +6,8 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  use "BurntSushi/ripgrep"
+
   use {
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
