3f8f5a412d4ba1be374afc6728c975102ac7e966
Brock Peabody <brock.peabody@gmail.com>
2024-09-22 11:36:26 -0500

    some more stuff added

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -435,3 +435,39 @@ require("aerial").setup({
 })
 -- You probably also want to set a keymap to toggle aerial
 vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+
+
+local null_ls = require("null-ls")
+
+-- code action sources
+local code_actions = null_ls.builtins.code_actions
+
+-- diagnostic sources
+local diagnostics = null_ls.builtins.diagnostics
+
+-- formatting sources
+local formatting = null_ls.builtins.formatting
+
+-- hover sources
+local hover = null_ls.builtins.hover
+
+-- completion sources
+local completion = null_ls.builtins.completion
+
+local sources = {
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.completion.spell,
+    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.diagnostics.write_good,
+    null_ls.builtins.code_actions.gitsigns,
+    require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+}
+
+null_ls.setup({
+    code_actions = code_actions,
+    diagnostics = diagnostics,
+    formatting = formatting,
+    hover = hover,
+    completion = completion,
+    sources = sources,
+})
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -120,4 +120,7 @@ return {
 
   { "stevearc/aerial.nvim" },
 
+  { "lewis6991/gitsigns.nvim"}, -- required for the below
+  { "nvimtools/none-ls.nvim" },
+  { "nvimtools/none-ls-extras.nvim" },
 }
